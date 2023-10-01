a95e036509612a80b96cd9f2e8b78d811c8ad337
Nat <nathan.hamon.44@gmail.com>
2023-10-01 17:50:04 +0200

    setting up the level design section with the accordeon menu system. Video editing and scripting is ok

--- main.js
+++ main.js
@@ -1,3 +1,53 @@
 
 import "./css/normelize.css";
 import './css/style.css';
+
+
+
+
+const accordeonButtons = document.querySelectorAll(".button-accordeon");
+
+
+for(let button of accordeonButtons)
+{
+    button.addEventListener("click", (e)=>
+    {
+        const accordeonContent = document.querySelector(`button[id="${button.id}"] + article`);
+
+        const video = document.querySelector(`button[id="${button.id}"] video`);
+       
+        button.classList.toggle("button-active");
+
+        if(!video.src && button.id != 2)
+        {
+
+            video.src = `./video/map${button.id}_opening.mp4`;
+            video.loop = false;
+            video.muted = false;
+
+            setTimeout(()=>
+            {
+                video.poster = `./image/map${button.id}_open_idle_frame.png`;
+
+            }, 1000);
+            
+            
+
+            video.addEventListener("ended", ()=>
+            {
+                video.src = `./video/map${button.id}_open_idle.mp4`;
+                video.loop = true;
+                video.muted = true;
+
+            });
+
+
+           
+        }
+
+        
+
+        accordeonContent.classList.toggle("active");
+
+    })
+}
