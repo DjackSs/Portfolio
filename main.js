43ddf92cf4df4fe55509899378cff66aead66dd0
Nat <nathan.hamon.44@gmail.com>
2023-10-02 19:17:31 +0200

    seting up somme canvas animation, improvements are on the way

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ import "./css/normelize.css";
 import './css/style.css';
 
 
+// ============================================
+// ACCORDEON MENU
+// ============================================
 
 
 const accordeonButtons = document.querySelectorAll(".button-accordeon");
@@ -40,14 +43,135 @@ for(let button of accordeonButtons)
                 video.muted = true;
 
             });
-
-
-           
+          
         }
-
-        
 
         accordeonContent.classList.toggle("active");
 
     })
 }
+
+
+// ============================================
+// CANVAS HEADER
+// ============================================
+
+const header = document.querySelector("header");
+
+
+const canvas = document.querySelector("canvas");
+
+const ctx = canvas.getContext("2d");
+
+const canvasWidth = header.getBoundingClientRect().width;
+
+canvas.width = canvasWidth;
+
+const canvasHeight = header.getBoundingClientRect().height;
+
+canvas.height = canvasHeight;
+
+
+
+ctx.fillStyle = "#242424";
+ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+
+ctx.fillStyle = "#646cff";
+
+
+class ball
+{
+    constructor (effect)
+    {
+        this.effect = effect;
+        this.radius = Math.random()* 80 + 50;
+        this.x = Math.random() * this.effect.width;
+        this.y = this.effect.height+this.radius;
+        this.speedX = Math.random() - 0.5;
+        this.speedY = Math.random() * -0.5;
+        
+    }
+    update ()
+    {
+        if(this.x - this.radius <= 0 || this.x + this.radius >= this.effect.width)
+        {
+            this.speedX *= -1;
+        }
+
+        if(this.y  <= 0 - this.radius)
+        {
+            this.y = this.effect.height + this.radius;
+        }
+
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+
+    draw (context)
+    {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+        context.fill();
+    }
+
+   
+}
+
+
+class MetaballsEffect
+{
+    constructor (width, height)
+    {
+        this.width = width;
+        this.height = height;
+        this.metaballsArray= [];
+
+    }
+
+    init (numberOfBalles)
+    {
+        for(let i=0 ; i< numberOfBalles; i++)
+        {
+            this.metaballsArray.push(new ball(this));
+        }
+    }
+
+    update ()
+    {
+        this.metaballsArray.forEach(metaball => metaball.update());
+
+    }
+
+    draw (context)
+    {
+        this.metaballsArray.forEach(metaball => metaball.draw(context));
+        
+    }
+}
+
+
+
+const test = new MetaballsEffect(canvasWidth, canvasHeight);
+test.init(20);
+
+
+
+function animate ()
+{
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    ctx.fillStyle = "#242424";
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    ctx.fillStyle = "#646cff";
+    
+    test.update();
+    test.draw(ctx);
+
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+
